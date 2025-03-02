3475059aafb9f022aadb7e55a31967376e1aa7d5
sccsc1342025 <sebastiancam711@gmail.com>
2025-03-02 20:01:19 +0000

    csc134_M3LAB1.cpp

--- csc134_M3LAB1.cpp
+++ csc134_M3LAB1.cpp
@@ -1,16 +1,47 @@
 // CSC 134
-// M3T1
+// M3LAB1
 // Sebastian Camacho
-// 02/22/2025
+// 03/2/2025
 
 #include <iostream>
-
 using namespace std;
+
+void chooseBottle1();
+void chooseBottle2();
 
 int main()
 {
-    // variables
-    
+    // this program will let you choose between two bottels, one poisoned one not
+    int choice;
+
+    // choose a bottle
+    cout << "There are two bottles in front of you. One is posioned one is not, pick one " << endl;
+    cout << "Will you choose Bottle 1 or Bottle 2" << endl;
+    cout << "Type 1 or 2" << endl;
+    cin >> choice;
+
+    if (1 == choice) {
+        chooseBottle1();
+    }
+    else if (2 == choice) {
+        chooseBottle2();
+    }
+    else {
+        cout << "I'm sorry that is not a valid choice" << endl;
+    }
+
+    cout << "Thank you for playing!" << endl;
 
     return 0;
+}
+
+// defining orginal variables
+void chooseBottle1() {
+    cout << "You chose Bottle 1" << endl;
+    cout << "Congradualations you get to live!" << endl;
+}
+
+void chooseBottle2() {
+    cout << "You chose Bottle 2" << endl;
+    cout << "Unfortunately you will die a painful death! Goodbye." << endl;
 }
